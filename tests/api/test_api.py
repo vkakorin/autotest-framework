7a8fb4101322f1a5e022b5d6b69b8efe8791f9a0
Viktor Kakorin <vkakorin@gmail.com>
2023-01-29 07:59:34 +0200

    Added aditional tests for checking of changing first name

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -5,3 +5,13 @@ import pytest
 def test_remove_name(user):
     user.name = ""
     assert user.name == ""
+
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == "Viktor"
+
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == "Kakorin"
