94bb7cf1fc7c7331359f4d3106930d68b6f8ebff
Viktor Kakorin <vkakorin@gmail.com>
2023-01-29 07:45:40 +0200

    Added test for checking first second name of user

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.mark.check
 def test_change_name(user):
     assert user.name == 'Viktor'
+
+
+@pytest.mark.check
+def test_change_second_name(user):
+    assert user.second_name == 'Kakorin'
